c2316ec96e715a816faeb7337e65ff1f3d2eb8d1
GabrielAmponsaKoranteng <korantenggabby@gmail.com>
2021-05-21 03:10:58 -0800

    Added content

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import MenuComponent from './MenuComponent';
+import LoginComponent from './LoginComponent';
+
+function App() {
+  return (
+    <div class="page-container">
+      <MenuComponent />
+      <LoginComponent />
+    </div>
+  );
+}
+
+export default App;
